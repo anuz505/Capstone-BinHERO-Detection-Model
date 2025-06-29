dd820cd996bbac2b587b6286d1a32e10c5d5eaa0
anuj <anujb364@gmail.com>
2025-06-29 08:35:08 +0545

    first commit

---
+++ capstone/main.py
@@ -0,0 +1,75 @@
+import cv2
+import argparse
+from ultralytics import YOLO
+import supervision as sv
+import time
+def parse_arguments()-> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Yolov11 live")
+    parser.add_argument("--webcam-resolution",default=[1280,720],nargs=2,type=int)
+    args = parser.parse_args()
+    return args
+
+
+
+def main():
+    args = parse_arguments()
+    frame_width, frame_height = args.webcam_resolution
+    cap = cv2.VideoCapture(0)  
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    cap.set(cv2.CAP_PROP_FPS,45)
+    
+    if not cap.isOpened():
+        print("Error: Could not open camera")
+        return
+    model = YOLO("../best.pt")
+
+    box_annotator = sv.BoxAnnotator(
+        thickness=2
+    )
+    label_annotator = sv.LabelAnnotator(
+        text_thickness=2,
+        text_scale=1,
+        text_color=sv.Color.BLACK,
+        text_position=sv.Position.TOP_LEFT
+    )
+
+    confidence_threshold = 0.5
+    max_box_area = 0.7
+    pTime = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not read frame")
+            break
+        result = model(frame, agnostic_nms=True)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        
+        #  labels with class names and confidence scores
+        labels = [
+            f"{model.names[class_id]} {confidence:.2f}"
+            for class_id, confidence in zip(detections.class_id, detections.confidence)
+        ]
+
+        frame = box_annotator.annotate(scene=frame, detections=detections)
+        frame = label_annotator.annotate(scene=frame, detections=detections, labels=labels)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(frame, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+       
+        cv2.imshow("yolo11l", frame)
+        
+        
+        if cv2.waitKey(30) == 27:  # 27 bhaneko esc key
+            break
+    
+    print(frame.shape)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
